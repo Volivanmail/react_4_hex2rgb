a0f866a8fe1216a7960898032349d0e2d0e52b7c
Your Name <you@example.com>
2023-06-15 01:09:25 +0300

    commit 14.06

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
 
 function App() {
 
   return (
-    <>
-    </>
+  <h2>Color Converter
+    <div className='container'>
+      <form className='form'>
+        <label htmlFor="HEX">HEX</label>
+        <input className="input" type="text" id="HEX" name="HEX" placeholder='Введите код цвета, например: #000000'></input>
+        <label htmlFor="rgb">RGB</label>
+        <input className="input" type="text" id="rgb" name="rgb"></input>
+      </form>
+    </div>
+  </h2>
   )
 }
 
